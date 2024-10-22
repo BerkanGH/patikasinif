8afd337fc348e3471d4f2d44f03c6dd248de6c24
BerkanGH <berkankeskin28@gmail.com>
2024-10-22 16:30:03 +0300

    s

---
+++ Patikasınıf/Patikasınıf/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patikasınıf
+{
+    public class Person
+    {
+        private string name;
+
+        private string soyad;
+
+        private DateTime dogumGunu;
+
+        public string Name { get { return name; } set { name = value; } }
+        public string Soyad { get { return soyad; } set { soyad = value; } }
+        public DateTime DogumGunu { get { return dogumGunu; } set { dogumGunu = value; } }
+
+        public void bilgileriGöster() {
+
+            Console.WriteLine($"adı : {name } soyadı: {soyad} doğum günü : {dogumGunu}");
+        
+        
+        }
+
+
+
+
+    }
+
+}
---
+++ Patikasınıf/Patikasınıf/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Patikasınıf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Person ogrenci1 = new Person();
+            ogrenci1.Name = "ali";
+            ogrenci1.Soyad = "gürbüz";
+            ogrenci1.DogumGunu = new DateTime(2005, 7, 18);
+
+            Person ogretmen1 = new Person();
+            ogretmen1.Name = "mehmet";
+            ogretmen1.Soyad = "karahanlı";
+            ogretmen1.DogumGunu = new DateTime(1960,1,1);
+
+            ogrenci1.bilgileriGöster();
+            ogretmen1.bilgileriGöster();
+
+
+        }
+    }
+}
